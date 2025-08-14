c424096184fdbeb334670e4e991c321f0588be36
Hariganesh V <hariganeshv2006@gmail.com>
2025-08-14 09:06:59 +0530

    Update decimal_to_binary.py
    retain the same order of remainders for trial

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -11,10 +11,11 @@ def decimal_to_binary(decimal_num):
      num = num // 2
      
     #The binary digits are stored in reverse order
-   binary_str = ''.join(reversed(binary_digits))
-   return binary_str
+   #binary_str = ''.join(reversed(binary_digits))
+   return binary_digits
     
 #Example usage:
 print(decimal_to_binary(10)) #Output:"0101"
 print(decimal_to_binary(0)) #Output:"0"
 print(decimal_to_binary(4)) #Output:"001"
+
