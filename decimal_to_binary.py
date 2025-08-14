f6bb6945e8c21465cf6cf4cc80a48161b6fc3a1d
Hariganesh V <hariganeshv2006@gmail.com>
2025-08-14 09:04:55 +0530

    Add files via upload

---
+++ decimal_to_binary.py
@@ -0,0 +1,20 @@
+def decimal_to_binary(decimal_num):
+   if decimal_num == 0:
+     return "0"
+     
+   binary_digits = []
+   num = decimal_num
+   
+   while num > 0:
+     reminder = num % 2
+     binary_digits.append(str(reminder))
+     num = num // 2
+     
+    #The binary digits are stored in reverse order
+   binary_str = ''.join(reversed(binary_digits))
+   return binary_str
+    
+#Example usage:
+print(decimal_to_binary(10)) #Output:"0101"
+print(decimal_to_binary(0)) #Output:"0"
+print(decimal_to_binary(4)) #Output:"001"
